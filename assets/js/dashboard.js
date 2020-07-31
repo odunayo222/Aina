e38e48dc313d63ed22cc4d95d6b2b38dac73c195
odunayo222 <mayowaakanke@gmail.com>
2020-07-31 22:03:10 +0100

    dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,11 +3,14 @@ const handleProfile = document.querySelector('#handleProfile');
 
 window.addEventListener('load', () => {
   // assign variable to display user data dynamically
-  const dynamicUsername = document.getElementById('dynamic_username');
-  const dynamic_tel = document.getElementById('dynamic_tel');
-  const dynamic_email = document.querySelector("#dynamic_email")
-  const dynamic_address = document.getElementById('dynamic_address');
-
+  const dynamicUsernameLg = document.getElementById('dynamic_username-lg');
+  const dynamicUsernameSm = document.getElementById('dynamic_username-sm');
+  const dynamic_telLg = document.getElementById('dynamic_tel-lg');
+  const dynamic_telSm = document.getElementById('dynamic_tel-sm');
+  const dynamic_emailLg = document.querySelector("#dynamic_email-lg");
+  const dynamic_emailSm = document.querySelector("#dynamic_email-sm");
+  const dynamic_addressLg = document.getElementById('dynamic_address-lg');
+  const dynamic_addressSm = document.getElementById('dynamic_address-sm');
 
   // get the values from localstorage and store in a variable
   // using the key names
@@ -25,9 +28,12 @@ window.addEventListener('load', () => {
     window.location.href = './login.html';
   }else{
     // if it has update dashboard page dynamically from the values gotten
-    dynamicUsername.textContent = username;
-    dynamic_tel.textContent = telephone;
-    dynamic_address.textContent = address;
+    dynamicUsernameLg.textContent = username;
+    dynamicUsernameSm.textContent = username;
+    dynamic_telLg.textContent = telephone;
+    dynamic_telSm.textContent = telephone;
+    dynamic_addressLg.textContent = address;
+    dynamic_addressSm.textContent = address;
     
 
   }
